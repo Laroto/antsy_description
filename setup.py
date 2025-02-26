83b11972d9803063865c28d8030ab7fcfbd8c01a
Pedro Rocha <pedrosr711@gmail.com>
2025-02-26 13:32:20 +0100

    save local changes in temp branch

--- setup.py
+++ setup.py
@@ -1,3 +1,49 @@
+# import os
+# from setuptools import setup
+# from glob import glob
+
+# def collect_files_with_paths(base_dir, install_base):
+#     """Recursively collect all files in a directory, preserving subdirectory structure."""
+#     collected_files = []
+#     for root, _, files in os.walk(base_dir):
+#         for file in files:
+#             # Calculate the install directory relative to the base directory
+#             install_path = os.path.join(install_base, os.path.relpath(root, base_dir))
+#             # Add file and its target installation path
+#             collected_files.append((install_path, [os.path.join(root, file)]))
+#     return collected_files
+
+# package_name = 'antsy_description'
+
+# data_files = [
+#     ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+#     ('share/' + package_name, ['package.xml']),
+#     (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+#     (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+#     (os.path.join('share', package_name, 'config'), glob('config/*')),
+# ]
+
+
+# # Add all files in the 'meshes' folder
+# data_files.extend(collect_files_with_paths('meshes', f'share/{package_name}/meshes'))
+
+# setup(
+#     name=package_name,
+#     version='0.0.0',
+#     packages=[package_name],
+#     data_files=data_files,
+#     install_requires=['setuptools'],
+#     zip_safe=True,
+#     maintainer='Pedro Rocha',
+#     maintainer_email='pedrosr711@gmail.com',
+#     description='The ' + package_name + ' package',
+#     license='MIT',
+#     tests_require=['pytest'],
+#     entry_points={
+#         'console_scripts': [],
+#     },
+# )
+
 import os
 from setuptools import setup
 from glob import glob
@@ -15,15 +61,22 @@ def collect_files_with_paths(base_dir, install_base):
 
 package_name = 'antsy_description'
 
+# Filter function to check for regular files.
+def is_file(path):
+    return os.path.isfile(path)
+
+# Only include files (ignore directories) in the urdf folder.
+urdf_files = [f for f in glob('urdf/*') if is_file(f)]
+
 data_files = [
     ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
     ('share/' + package_name, ['package.xml']),
     (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-    (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+    (os.path.join('share', package_name, 'urdf'), urdf_files),
     (os.path.join('share', package_name, 'config'), glob('config/*')),
 ]
 
-# Add all files in the 'meshes' folder
+# Add all files in the 'meshes' folder recursively.
 data_files.extend(collect_files_with_paths('meshes', f'share/{package_name}/meshes'))
 
 setup(
